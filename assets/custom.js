3f5f46b6f3aa455b28ed9394c1dfcbb52e80c40d
Sameh Sadaka <samsadaka@gmail.com>
2025-05-15 16:01:00 +0300

    Init

---
+++ assets/custom.js
@@ -0,0 +1,21 @@
+/*
+* 01 Theme
+*
+* Javascript in this fill will make it easier to update. In order
+* to use this file you will need to open layout/theme.liquid and uncomment
+* the custom.js script import line near the bottom of the file.
+*/
+
+
+(function() {
+  // Add custom code below this line
+
+
+  
+
+
+
+
+  // ^^ Keep your scripts inside this IIFE function call to 
+  // avoid leaking your variables into the global scope.
+})();
